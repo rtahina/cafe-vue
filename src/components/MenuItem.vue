f56b39376ca70d0b24b994687b6acd1566eca3e2
Tahina R <to.tahina@gmail.com>
2021-09-24 22:39:51 +0300

    Cafe vue cli with a MenuItem component

---
+++ src/components/MenuItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="menu-item">
+        <img
+            class="menu-item__image"
+            :src="imgSource"
+            :alt="imgAlt"
+        />
+        <div>
+            <h3>{{ name }}</h3>
+            <p v-if="inStock">En stock</p>
+            <p v-else>En rupture de stock</p>
+            <div>
+                <label for="add-item-quantity"
+                    >Quantité : {{ quantity }}</label
+                >
+                <input
+                    v-model.number="quantity"
+                    id="add-item-quantity"
+                    type="number"
+                />
+                <button @click="addToShoppingCart(quantity)">
+                    Ajouter au panier d'achat
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MenuItem',
+    props: {
+        name: String,
+        imgSource: String,
+        imgAlt: String,
+        quantity: Number,
+        inStock: Boolean
+    }
+}
+</script>
+
+<style>
+    .menu {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    }
+
+    .menu-item {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    margin-bottom: 30px;
+    }
+
+    .menu-item__image {
+    max-width: 300px;
+    }
+</style>
